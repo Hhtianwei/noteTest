45efdc5d8278fc8335c6549056a8b71c95658b87
guowangyunyan <924631391@qq.com>
2017-06-12 20:14:29 +0800

    代码CharTest.java

---
+++ yuanx/CharTest.java
@@ -0,0 +1,16 @@
+package com.shsxt.yx;
+
+public class CharTest {
+
+public static void main(String[] args) {
+	//×Ö·û²ÎÓëÔËËã
+	char c = 'a';
+	char c1 = '1';
+	System.out.println("c1=" + c1);
+	System.out.println(c1+1);
+	char c2 = '2' + 1;
+	System.out.println("c2=" + c2);
+	System.out.println("c = " + c + 1 );
+	}
+}
+
